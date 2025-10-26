eeb685c9e1a9afba608702947e533bde6933edef
nafe333 <94410012+nafe333@users.noreply.github.com>
2025-10-26 18:46:54 +0300

    Address cell button
    added a button to address cell that toggles between addresses

--- SHOPPIX/ChooseAddressVC/Cells/AddressTableViewCell.swift
+++ SHOPPIX/ChooseAddressVC/Cells/AddressTableViewCell.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class AddressTableViewCell: UITableViewCell {
+       //MARK: - Properties
+    var isAddressSelected: Bool = false
+    
    //MARK: - Outlets
     
     @IBOutlet weak var countryNameLabel: UILabel!
     
     @IBOutlet weak var specificAddressLabel: UILabel!
     
+    @IBOutlet weak var selectedAddressButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    
+    @IBAction func selectedAddressButtonTapped(_ sender: UIButton) {
+        isAddressSelected.toggle()
+        let buttonImageName = isAddressSelected ? "smallcircle.fill.circle.fill" : "circle"
+        selectedAddressButton.setImage(UIImage(systemName: buttonImageName), for: .normal)
     }
 }
